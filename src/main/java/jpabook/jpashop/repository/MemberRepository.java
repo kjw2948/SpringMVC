526a9f9b638a71758a22e079d5be7fb5d28e3075
kjw2948 <kjw970313@cau.ac.kr>
2024-05-02 16:23:45 +0900

    [Update] 회원레포지토리 구현

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -1,9 +1,11 @@
-package jpabook.jpashop.domain;
+package jpabook.jpashop.repository;
 
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.EntityManager;
 import jpabook.jpashop.domain.Member;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository // 컴포넌트 대상이 되는 어노테이션 중 하나
@@ -21,4 +23,14 @@ public class MemberRepository {
         return em.find(Member.class, id);
     }
 
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
 }
