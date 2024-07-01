489a9b0eb407611f07070806fdd691e53967b332
kjw2948 <kjw970313@cau.ac.kr>
2024-07-01 18:34:27 +0900

    [Update] update

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -1,4 +1,27 @@
 package jpabook.jpashop.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
 public class Category {
+    @Id
+    @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    @ManyToMany
+    @JoinTable(name = "category_item")
+    private List<Category> items = new ArrayList<>();
+
+
 }
