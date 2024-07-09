a69c9ac6cb90fd1216b79b7bd0c38daf12d79637
kjw2948 <kjw970313@cau.ac.kr>
2024-07-09 17:42:10 +0900

    [Update] Member Repository 구현

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -6,11 +6,14 @@ import org.springframework.stereotype.Repository;
 import jpabook.jpashop.domain.Member;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Repository
 @Transactional
 public class MemberRepository {
 
-    @PersistenceContext
+    @PersistenceContext // JPA 사용 -> 엔티티 매니저 필수!
     private EntityManager em;
 
     public Long save(Member member) {
@@ -18,7 +21,21 @@ public class MemberRepository {
         return member.getId();
     }
 
-    public Member find(Long id) {
+    public Member findOne(Long id) {
         return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() { // 전부 조회해야 할 때는 쿼리 생성 필요함 -> JPQL 쿼리
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+        // jpql => 엔티티객체를 대상으로 쿼리를 날림
+        // sql ==> 테이블을 대상으로 쿼리를 날림
+        // from에 대상이 엔티티 객체냐 테이블이냐 차이
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name) // Query에서 말하는 name이 어떤 name인지 setting 해주는 것
+                .getResultList();
     }
 }
