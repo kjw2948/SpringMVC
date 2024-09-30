fb14fc870bf3e32bb34577393af887ef5a6077d2
kjw2948 <kjw970313@cau.ac.kr>
2024-09-30 20:25:39 +0900

    [Update] Order 로직 구현

--- src/main/java/jpabook/jpashop/item/Album.java
+++ src/main/java/jpabook/jpashop/item/Album.java
@@ -2,8 +2,6 @@ package jpabook.jpashop.item;
 
 import jakarta.persistence.DiscriminatorValue;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
 import lombok.Getter;
 import lombok.Setter;
 
